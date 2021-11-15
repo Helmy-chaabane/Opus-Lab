3ef730b7e5cccf45725a4050fdf0651551ac0f96
Helmy-chaabane <Chaabane504@gmail.com>
2021-11-15 15:10:05 +0100

    adding person interface and AddPerson fuc

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
+import { useState } from "react";
 import "./app.scss";
+import StatusOptions from "./StatusOptions";
 
 const peaple = [
   "Franquelin Benjamin",
@@ -13,14 +15,35 @@ const peaple = [
   "Robert Julia",
 ];
 
+export enum Status {
+  ABSCENT,
+  PRENSEN,
+  EXCUSE,
+}
+interface Person {
+  name: string;
+  status: Status;
+}
+type AddPerson = (status: Status) => void;
+
 function App() {
+  const [persons, setPersons] = useState<Person[]>([]);
+  const [index, setIndex] = useState(0);
+
+  const addPerson: AddPerson = (status) => {
+    setPersons([...persons, { name: peaple[index], status }]);
+    setIndex(index + 1);
+  };
+
   return (
     <main className="main">
+      <h1>OPUS LAB TEST TECH</h1>
+      <StatusOptions addPerson={addPerson} />
       <ul className="main__list">
         {peaple.map((person: string) => (
           <li className="main__list__item">
             <p>{person}</p>
-            <span>Present</span>
+            <span>Absence</span>
           </li>
         ))}
       </ul>
---
+++ src/StatusOptions.tsx
@@ -0,0 +1,12 @@
+import { Status } from "./App";
+import { FC } from "react";
+
+const StatusOptions: FC = () => {
+  return (
+    <div className="main__statusOptions">
+      <div className=""></div>
+    </div>
+  );
+};
+
+export default StatusOptions;
