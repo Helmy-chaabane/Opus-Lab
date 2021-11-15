3e2ef284ecdd40717f0724a0f65c9f583f82bf69
Helmy-chaabane <Chaabane504@gmail.com>
2021-11-15 14:38:14 +0100

    list preparation

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,31 @@
-import "./App.css";
+import "./app.scss";
+
+const peaple = [
+  "Franquelin Benjamin",
+  "Dunord Caroline",
+  "Reacher Jack",
+  "Wilson Wade",
+  "Pool Francis",
+  "Cesar Alex",
+  "Ten Ben",
+  "Guetta David",
+  "Kennedy John",
+  "Robert Julia",
+];
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <main className="main">
+      <ul className="main__list">
+        {peaple.map((person: string) => (
+          <li className="main__list__item">
+            <p>{person}</p>
+            <span>Present</span>
+          </li>
+        ))}
+      </ul>
+    </main>
+  );
 }
 
 export default App;
